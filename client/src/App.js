f857cd67af4624453d9902b3897e6b43d76590a4
Fuzail22 <102959853+Fuzail22@users.noreply.github.com>
2023-10-24 22:01:40 +0530

    SOP done and styles applied and tested

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,11 @@
 import "./App.css";
 import axios from "axios";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-
-let suppliersdata;
+import { useCallback, useMemo, useState } from "react";
+import Docket from "./DocketForm";
 
 function App() {
-  const [suppliers, setSuppliers] = useState(false);
-  const [po, setPO] = useState(false);
-  const [posdata, setPosdata] = useState([]);
-  // const [docket,setDocket]=useState(false);
+  const [docket, setDocket] = useState(false);
   const [dockets, setDockets] = useState([]);
-  const nameRef = useRef(null),
-    startRef = useRef(null),
-    endRef = useRef(null),
-    hoursRef = useRef(null),
-    rateRef = useRef(null),
-    supplierRef = useRef(null),
-    poRef = useRef(null);
 
   let getDockets = useCallback(() => {
     axios
@@ -32,91 +21,22 @@ function App() {
       });
   }, []);
   useMemo(() => getDockets(), [getDockets]);
-  useEffect(() => {
-    axios
-      .get("http://localhost:6001/allsuppliers")
-      .then((response) => {
-        suppliersdata = response.data;
-        console.log(suppliersdata);
-        setSuppliers(true);
-        // console.log(suppliers.map((sup) => sup.supplier));
-        // console.log(typeof suppliers);
-      })
-      .catch((err) => {
-        console.log(
-          "The following error occured while fetching suppliers: ",
-          err
-        );
-      });
-  }, []);
-  const getPOs = useCallback((e) => {
-    // console.log(e.target.value);
-    const suppName = e.target.value;
-    axios
-      .get(`http://localhost:6001/supplier?supp=${suppName}`)
-      .then((response) => {
-        console.log(response.data.purchaseOrders);
-        setPosdata(response.data.purchaseOrders);
-        setPO(true);
-        // console.log(suppliers.map((sup) => sup.supplier));
-        // console.log(typeof suppliers);
-      })
-      .catch((err) => {
-        console.log(
-          "The following error occured while fetching suppliers: ",
-          err
-        );
-      });
-  }, []);
-  const createHandler = useCallback((e) => {
-    e.preventDefault();
-    const [description, poNumber] = poRef.current.value.split(",");
-    console.log(nameRef.current.value);
-    console.log(startRef.current.value);
-    console.log(endRef.current.value);
-    console.log(hoursRef.current.value);
-    console.log(rateRef.current.value);
-    console.log(supplierRef.current.value);
-    console.log(description);
-    console.log(poNumber);
-    const formData = {
-      name: nameRef.current.value,
-      start_time: startRef.current.value,
-      end_time: endRef.current.value,
-      no_of_hours_worked: hoursRef.current.value,
-      rate_per_hour: rateRef.current.value,
-      supplierName: supplierRef.current.value,
-      description: description,
-      po_nummber: poNumber,
-    };
-    axios
-      .post(`http://localhost:6001/docket`, formData)
-      .then((response) => {
-        console.log(response);
-        document.getElementById("docketForm").reset();
-      })
-      .catch((err) => {
-        console.log(
-          "The following error occured while creating the docket: ",
-          err
-        );
-      });
-  }, []);
 
   return (
     <div className="App">
       <header>
-        <button>Create Docket</button>
+        <h1>Welcome to Purchase Processor Web App</h1>
+        <button onClick={() => setDocket(true)}>Create Docket</button>
       </header>
       <table>
         <thead>
           <tr>
             <th>Name</th>
             <th>Start time</th>
-            <th>End time:</th>
+            <th>End time</th>
             <th>Hours Worked</th>
             <th>Rate Per Hour</th>
-            <th>supplierName</th>
+            <th>Supplier Name</th>
             <th>Purchase Order</th>
             <th>PO Number</th>
           </tr>
@@ -136,70 +56,13 @@ function App() {
           ))}
         </tbody>
       </table>
-      <form id="docketForm" onSubmit={createHandler}>
-        <label>
-          Name:
-          <input type="text" ref={nameRef} required></input>
-        </label>
-        <label>
-          Start time:
-          <input type="time" ref={startRef} required></input>
-        </label>
-        <label>
-          End time:
-          <input type="time" ref={endRef} required></input>
-        </label>
-        <label>
-          Hours Worked
-          <input type="number" ref={hoursRef} required></input>
-        </label>
-        <label>
-          Rate Per Hour
-          <input type="text" ref={rateRef} required></input>
-        </label>
-        {suppliers && (
-          <label>
-            Supplier:
-            <select
-              onChange={getPOs}
-              defaultValue={"DEFAULT"}
-              ref={supplierRef}
-              required
-            >
-              <option value={"DEFAULT"} hidden>
-                Select the Supplier
-              </option>
-              {suppliersdata.map((sup) => {
-                return (
-                  <option key={sup.supplier} value={sup.supplier}>
-                    {sup.supplier}
-                  </option>
-                );
-              })}
-            </select>
-          </label>
-        )}
-        {po && (
-          <label>
-            PO:
-            <select defaultValue={"DEFAULT"} ref={poRef} required>
-              <option value={"DEFAULT"} hidden>
-                Select the Description
-              </option>
-              {posdata.map((po) => {
-                return (
-                  <option key={po._id} value={[po.Description, po.PO_Number]}>
-                    {po.Description}-{po.PO_Number}
-                  </option>
-                );
-              })}
-            </select>
-          </label>
-        )}
-        {/* <input type="submit" /> */}
-        <button type="submit">Save</button>
-        <button type="button">Cancel</button>
-      </form>
+      {docket && (
+        <div className="Overlay">
+          <div className="overlay-form">
+            <Docket setDocket={setDocket} getDockets={getDockets} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- client/src/DocketForm.js
+++ client/src/DocketForm.js
@@ -1,10 +1,199 @@
-function Docket() {
+import { useCallback, useRef, useEffect, useState } from "react";
+import axios from "axios";
+let suppliersdata;
+function Docket(props) {
+  const [suppliers, setSuppliers] = useState(false);
+  const [po, setPO] = useState(false);
+  const [posdata, setPosdata] = useState([]);
+
+  const nameRef = useRef(null),
+    startRef = useRef(null),
+    endRef = useRef(null),
+    hoursRef = useRef(null),
+    rateRef = useRef(null),
+    supplierRef = useRef(null),
+    poRef = useRef(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:6001/allsuppliers")
+      .then((response) => {
+        suppliersdata = response.data;
+        console.log(suppliersdata);
+        setSuppliers(true);
+        // console.log(suppliers.map((sup) => sup.supplier));
+        // console.log(typeof suppliers);
+      })
+      .catch((err) => {
+        console.log(
+          "The following error occured while fetching suppliers: ",
+          err
+        );
+      });
+  }, []);
+
+  const getPOs = useCallback((e) => {
+    // console.log(e.target.value);
+    const suppName = e.target.value;
+    axios
+      .get(`http://localhost:6001/supplier?supp=${suppName}`)
+      .then((response) => {
+        console.log(response.data.purchaseOrders);
+        setPosdata(response.data.purchaseOrders);
+        setPO(true);
+        // console.log(suppliers.map((sup) => sup.supplier));
+        // console.log(typeof suppliers);
+      })
+      .catch((err) => {
+        console.log(
+          "The following error occured while fetching suppliers: ",
+          err
+        );
+      });
+  }, []);
+  const createHandler = useCallback((props, e) => {
+    e.preventDefault();
+    const [description, poNumber] = poRef.current.value.split(",");
+    console.log(nameRef.current.value);
+    console.log(startRef.current.value);
+    console.log(endRef.current.value);
+    console.log(hoursRef.current.value);
+    console.log(rateRef.current.value);
+    console.log(supplierRef.current.value);
+    console.log(description);
+    console.log(poNumber);
+    const formData = {
+      name: nameRef.current.value,
+      start_time: startRef.current.value,
+      end_time: endRef.current.value,
+      no_of_hours_worked: hoursRef.current.value,
+      rate_per_hour: rateRef.current.value,
+      supplierName: supplierRef.current.value,
+      description: description,
+      po_nummber: poNumber,
+    };
+    axios
+      .post(`http://localhost:6001/docket`, formData)
+      .then((response) => {
+        console.log(response);
+        document.getElementById("docketForm").reset();
+        props.setDocket(false);
+        props.getDockets();
+      })
+      .catch((err) => {
+        console.log(
+          "The following error occured while creating the docket: ",
+          err
+        );
+      });
+  }, []);
   return (
-    <>
-      <label>
+    <form id="docketForm" onSubmit={(event) => createHandler(props, event)}>
+      <label className="overlay-label">
         Name:
-        <input type="text"></input>
+        <input
+          className="overlay-input"
+          type="text"
+          ref={nameRef}
+          required
+        ></input>
       </label>
-    </>
+      <label className="overlay-label">
+        Start time:
+        <input
+          className="overlay-input"
+          type="time"
+          ref={startRef}
+          required
+        ></input>
+      </label>
+      <label className="overlay-label">
+        End time:
+        <input
+          className="overlay-input"
+          type="time"
+          ref={endRef}
+          required
+        ></input>
+      </label>
+      <label className="overlay-label">
+        Hours Worked
+        <input
+          className="overlay-input"
+          type="number"
+          ref={hoursRef}
+          required
+        ></input>
+      </label>
+      <label className="overlay-label">
+        Rate Per Hour
+        <input
+          className="overlay-input"
+          type="text"
+          ref={rateRef}
+          required
+        ></input>
+      </label>
+      {suppliers && (
+        <label className="overlay-label">
+          Supplier:
+          <select
+            className="overlay-input"
+            onChange={getPOs}
+            defaultValue={"DEFAULT"}
+            ref={supplierRef}
+            required
+          >
+            <option value={"DEFAULT"} hidden>
+              Select the Supplier
+            </option>
+            {suppliersdata.map((sup) => {
+              return (
+                <option key={sup.supplier} value={sup.supplier}>
+                  {sup.supplier}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+      )}
+      {po && (
+        <label className="overlay-label">
+          PO:
+          <br />
+          <select
+            className="overlay-input"
+            defaultValue={"DEFAULT"}
+            ref={poRef}
+            required
+          >
+            <option value={"DEFAULT"} hidden>
+              Select the Description
+            </option>
+            {posdata.map((po) => {
+              return (
+                <option key={po._id} value={[po.Description, po.PO_Number]}>
+                  {po.Description}-{po.PO_Number}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+      )}
+      <div className="overlay-buttons">
+        <button type="submit" className="overlay-button">
+          Save
+        </button>
+        <button
+          type="button"
+          className="overlay-button"
+          onClick={() => props.setDocket(false)}
+        >
+          Cancel
+        </button>
+      </div>
+    </form>
   );
 }
+
+export default Docket;
